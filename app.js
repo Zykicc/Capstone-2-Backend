0e71882100acb81196b8f69efaaf07dd452f14fb
Zykicc <isaad3.one@gmail.com>
2024-05-14 18:34:16 -0400

    finihsed backend

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { BadRequestError } = require("./expressError");
 const cors = require("cors");
 
 const app = express();
@@ -21,14 +22,58 @@ const fetchData = async (key, steamId) => {
       redirect: "follow",
     }
   );
+  if (!result.ok) {
+    return new BadRequestError("Invalid Steam Id");
+  }
   const res = await result.json();
   // console.log(result);
   return res;
 };
 
-app.get("/", async (req, res) => {
-  const results = await fetchData(KEY, USER_ID);
-  // console.log(results);
+const fetchGameData = async (appId) => {
+  const result = await fetch(
+    `http://store.steampowered.com/api/appdetails?appids=${appId}`,
+    {
+      method: "GET",
+      redirect: "follow",
+    }
+  );
+  const res = await result.json();
+  return res;
+};
+
+const fetchProfileData = async (key, steamId) => {
+  const result = await fetch(
+    `http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${key}&steamids=${steamId}`,
+    {
+      method: "GET",
+      redirect: "follow",
+    }
+  );
+  const res = await result.json();
+  return res;
+};
+
+app.get("/getProfileData/:steamId", async (req, res) => {
+  const steamId = req.params.steamId;
+  const results = await fetchProfileData(KEY, steamId);
+  res.send(results);
+});
+
+app.get("/getOwnedGames/:steamId", async (req, res, next) => {
+  try {
+    const steamId = req.params.steamId;
+    const results = await fetchData(KEY, steamId);
+    res.send(results);
+  } catch (e) {
+    return next(e);
+  }
+});
+
+app.get("/getGameInfo/:appId", async (req, res) => {
+  const appId = req.params.appId;
+  const results = await fetchGameData(appId);
+
   res.send(results);
 });
 
